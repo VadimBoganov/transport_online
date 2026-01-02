b3b46f67048aedf7c59f287236314f4847dcba57
vadimboganov <boganov.v@tn.ru>
2026-01-02 12:12:50 +0300

    setup config

---
+++ config.ts
@@ -0,0 +1,27 @@
+export default {
+  map: {
+    lat: 54.628723,
+    lng: 39.716815,
+    zoom: 14,
+    stationVisibleZoom: 15,
+  },
+  routes: [
+    {
+      title: 'Автобусы',
+      type: 'А',
+      color: 'green',
+    },
+    {
+      title: 'Троллейбусы',
+      type: 'Т',
+      color: 'blue',
+    },
+    {
+      title: 'Маршрутные такси',
+      type: 'М',
+      color: '#ff6a00',
+    },
+  ],
+  routeIconSize: 18,
+  routeLineWeight: 3,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <div className="container mt-4">
-      <h1 className="text-primary">Bootstrap работает!</h1>
-      <button className="btn btn-success">Кнопка</button>
+      
     </div>
   );
 }
